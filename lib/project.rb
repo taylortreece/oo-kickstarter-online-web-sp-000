6c352bca5d19d895d9305c7451ae9ac488f25b31
Taylor Treece <taylor.treece@protonmail.com>
2021-01-22 09:37:50 -0600

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+class Project
+
+attr_reader :backers, :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self unless backer.backed_projects.include?(self)
+  end
+
+
+
+
+
+end
